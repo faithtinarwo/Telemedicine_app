038f77d3958214cacf7a747f5e8868fef8f68ed5
Faith <fayyic@gmail.com>
2024-11-13 12:34:28 +0200

    Run scheduler in the background

--- app.py
+++ app.py
@@ -230,4 +230,4 @@ def chat():
 # Run scheduler in the background
 if __name__ == '__main__':
     scheduler.start()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
